a084da931819a862c6b033b1acd625305ec30d3e
EC2 Default User <ec2-user@ip-172-31-29-110.us-east-2.compute.internal>
2018-05-26 15:09:56 +0000

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,11 @@ users = User.order(:created_at).take(6)
                                               fat: 10,
                                               carbohydrate: 70) }
 end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
